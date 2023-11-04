85d476e29a23df54a3513b00dcf096ddbd368dc4
qhuong1008 <qhuong1008@gmail.com>
2023-11-04 20:02:20 +0700

    update tag model

--- model/tag.model.js
+++ model/tag.model.js
@@ -2,7 +2,6 @@ const { Schema, model } = require("mongoose");
 const tag = new Schema({
   name: { type: String },
   description: { type: String },
-  is_active: { type: Boolean },
 });
 
 module.exports = model("tag", tag);
