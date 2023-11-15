8442aa9181e97062b4561c5a5e84a9b32c915fa8
qhuong1008 <qhuong1008@gmail.com>
2023-11-15 22:12:23 +0700

    get membership by user id

--- controller/membership.controller.js
+++ controller/membership.controller.js
@@ -1,7 +1,7 @@
 const membership = require("../model/membership.model");
 
 exports.create = async (req, res) => {
-  const isExist = await membership.findOne(req.body);
+  const isExist = await membership.findOne({ user: req.body.user });
   if (!isExist) {
     var membershipDetail = new membership(req.body);
     await membershipDetail
@@ -19,7 +19,18 @@ exports.findAll = async (req, res) => {
   const result = await membership.find({});
   res.json({ memberships: result, statusCode: 200 });
 };
-
+exports.findById = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const result = await membership.findOne({
+      user: userId,
+    });
+    res.json({ membership: result, statusCode: 200 });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: "Server error" });
+  }
+};
 exports.findOne = (req, res) => {};
 
 exports.update = (req, res) => {};
--- route/membership.route.js
+++ route/membership.route.js
@@ -5,6 +5,7 @@ var cors = require("cors");
 const membership = require("../controller/membership.controller");
 
 router.get("/get-membership", cors(), membership.findAll);
+router.get("/get-membership/:id", cors(), membership.findById);
 
 router.post("/add-membership", cors(), membership.create);
 
