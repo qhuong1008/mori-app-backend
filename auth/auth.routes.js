fff657dfedd563e28f2a086794d406927a24fbd5
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2023-11-24 20:35:07 +0700

    verify email when register account

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authController = require('./auth.controller');
-const fogotController = require('./forgotPassword.controller');
+const authController = require("./auth.controller");
+const fogotController = require("./forgotPassword.controller");
 
-router.post('/register', authController.registerAccount);
-router.post('/login', authController.login);
+router.post("/register", authController.registerAccount);
+router.post("/login", authController.login);
+
+// verify email
+router.get("/verify-email", authController.verifyEmail);
 
 // forgot password
-router.post('/forgot-password',  fogotController.forgotPassword);
-router.post('/reset-password', fogotController.resetPassword);
+router.post("/forgot-password", fogotController.forgotPassword);
+router.post("/reset-password", fogotController.resetPassword);
 
 module.exports = router;
