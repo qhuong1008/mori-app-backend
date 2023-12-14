f359a6696bb766b49e6c3f595311e952cb6fd1ac
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2023-12-15 03:29:10 +0700

    fix - readhistory

--- route/readHistory.route.js
+++ route/readHistory.route.js
@@ -6,7 +6,8 @@ const readHistory = require("../controller/readHistory.controller");
 
 router.get("/get-readHistory", cors(), readHistory.findAll);
 router.get("/get-readHistory/:id", cors(), readHistory.findAllWithUser);
+router.get("/get-readHistory/:book_id/:user_id", cors(), readHistory.findOne);
 
-router.post("/add-readHistory", cors(), readHistory.create);
+router.post("/add-readHistory", cors(), readHistory.createOrUpdateReadHistory);
 
 module.exports = router;
