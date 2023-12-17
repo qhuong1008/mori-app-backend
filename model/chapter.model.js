179d5fe7d1b56ddb05652cd4016481d3fd60f27c
qhuong1008 <qhuong1008@gmail.com>
2023-12-17 19:41:15 +0700

    Update chapter.model.js

--- model/chapter.model.js
+++ model/chapter.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const chapter = new Schema({
   book_id: { type: Schema.Types.ObjectId, ref: "Book" },
   name: { type: String, unique: true },
-  audio: { type: String, unique: true  },
+  audio: { type: String },
 });
 
 module.exports = model("Chapter", chapter);
