8488e3a0a1ae0e184d7663c0147ed5cb7d54dfe7
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2024-03-10 18:48:20 +0700

    update

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.DOMAIN,
   })
 );
 app.use(cookieParser());
