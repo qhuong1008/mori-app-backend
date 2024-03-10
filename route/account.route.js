076a4156dc10754510d69c4da6d60c620c9cf820
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2024-03-10 19:35:14 +0700

    update cors

--- route/account.route.js
+++ route/account.route.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const router = express.Router();
-var cors = require("cors");
+const cors = require("cors");
 const account = require("../controller/account.controller");
 
-router.get("/get-account", cors(), account.findAll);
-router.patch("/:id", cors(), account.update);
-router.get("/get-account/:id", cors(), account.findById);
-router.post("/add-account", cors(), account.create);
-router.post("/find-account", cors(), account.findOne);
-router.post("/change-password", cors(), account.changePassword);
+const whitelist = [process.env.DOMAIN]; // Thêm domain bạn muốn cho phép truy cập vào whitelist
 
-// route cho các api cần xác thực đăng nhập bằng username, password
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // Cho phép truy cập nếu origin nằm trong whitelist hoặc origin không được xác định (ví dụ: yêu cầu từ cùng một máy chủ)
+      callback(null, true);
+    } else {
+      // Từ chối truy cập nếu origin không nằm trong whitelist
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+router.get("/get-account", cors(corsOptions), account.findAll);
+router.patch("/:id", cors(corsOptions), account.update);
+router.get("/get-account/:id", cors(corsOptions), account.findById);
+router.post("/add-account", cors(corsOptions), account.create);
+router.post("/find-account", cors(corsOptions), account.findOne);
+router.post("/change-password", cors(corsOptions), account.changePassword);
+
+// Route cho các API cần xác thực đăng nhập bằng username, password
 const authMiddleware = require("../auth/auth.middlewares");
 const isAuth = authMiddleware.isAuth;
-router.get("/profile", isAuth, async (req, res) => {
+router.get("/profile", [cors(corsOptions), isAuth], async (req, res) => {
   res.send(req.user);
 });
 
--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(
   cors({
     origin: process.env.DOMAIN,
@@ -49,7 +48,6 @@ app.set("view engine", "hbs");
 
 // app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(logger("dev"));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
