ce5328ed5f604eb4f4908ab91234ab20fefbcf3d
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2024-07-17 23:38:04 +0700

    update

--- route/transaction.route.js
+++ route/transaction.route.js
@@ -6,6 +6,7 @@ const transaction = require("../controller/transaction.controller");
 
 router.get("/get-usertrans", cors(), transaction.getUserTrans);
 router.post("/add-transaction", cors(), transaction.create);
+router.get("/get-transaction/:book_id/:user_id", transaction.checkUserBuyBook);
 router.get("/get-transaction", cors(), transaction.getAllTransactions);
 router.get(
   "/get-by-date-range",
